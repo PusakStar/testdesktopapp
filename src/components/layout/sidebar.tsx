208e6f368a909b1137277506606d76978157a493
PusakStar <hiqal2003@gmail.com>
2025-08-16 22:38:52 +0800

    updated module abit

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,26 +1,18 @@
-import dreamicon from "../../assets/dreamicon.png";
-import LanguageIcon from "../../assets/language.png";
-import CustomerSupportIcon from "../../assets/cp.png";
 
-export default function loginsidebar() {
+
+export default function Sidebar() {
   return (
     <div className="sidebar">
-        <div className="dream-icon__wrapper">
-            <div className="circle-dreamicon">
-                <img src={dreamicon} alt="dreamicon" />
-            </div>
-            <h2>Dream</h2>
+      <div className="icontitle__wrapper">
+        <div className="iconcircle">
+
         </div>
-        <ul className="sidebar-item">
-          <li>
-            <img src={LanguageIcon} alt="LanguageIcon" />
-            <a href="#Language">English (US)</a>
-          </li>
-          <li>
-            <img src={CustomerSupportIcon} alt="CustomerSupportIcon" />
-            <a href="#CS">Customer Support</a>
-          </li>
-        </ul>
+      </div>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
     </div>
   );
 }
