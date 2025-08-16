cf58d08e27f9187957d4d311dbd01c2cb1d79009
PusakStar <hiqal2003@gmail.com>
2025-08-16 22:52:02 +0800

    lil changes

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,18 +1,26 @@
+import dreamicon from "../../assets/dreamicon.png";
+import LanguageIcon from "../../assets/language.png";
+import CustomerSupportIcon from "../../assets/cp.png";
 
-
-export default function Sidebar() {
+export default function loginsidebar() {
   return (
     <div className="sidebar">
-      <div className="icontitle__wrapper">
-        <div className="iconcircle">
-
+        <div className="dream-icon__wrapper">
+            <div className="circle-dreamicon">
+                <img src={dreamicon} alt="dreamicon" />
+            </div>
+            <h2>Dream</h2>
         </div>
-      </div>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+        <ul className="sidebar-item">
+          <li>
+            <img src={LanguageIcon} alt="LanguageIcon" />
+            <a href="#Language">English (US)</a>
+          </li>
+          <li>
+            <img src={CustomerSupportIcon} alt="CustomerSupportIcon" />
+            <a href="#CS">Customer Support</a>
+          </li>
+        </ul>
     </div>
   );
 }
